8801f1defae5cb8f4a8ec77e91a54ecb7f6a1e1b
Emir-Kazybaev <61088019+Emir-Kazybaev@users.noreply.github.com>
2020-06-07 18:20:12 +0200

    In case of encryption require user to enter a message to be less than n

--- src/RSA/Main.java
+++ src/RSA/Main.java
@@ -6,33 +6,39 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int p,q,fn,e,d;
+        BigInteger n;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your plaintext!");
         BigInteger msg = BigInteger.valueOf(scanner.nextInt());
-        int p = new RandomPrimeInt().getRandom();
-        int q = new RandomPrimeInt().getRandom();
-        BigInteger n = BigInteger.valueOf(p * q);
-        int fn = (p-1)*(q-1);
-        int e = 0;
-        int d = 0;
-        for (int i = 3; i < fn; i++){
-            e = i;
-            if (GCD.gcd(e,fn)==1)
-                break;
-        }
-        for(int i=0;i<=9;i++)
-        {
-            int x = 1+(i*fn);
-            if(x%e==0)
-            {
-                d=x/e;
-                break;
-            }
-        }
-        System.out.println("p = " + p + " and q = " + q + " n = " + n + " fn = " + fn + " e = " + e + " d = " + d);
         System.out.println("Do you want to encrypt(E) or decrypt(D) " + msg + "?");
         String path = scanner.next();
         if (path.equals("E")){
+            p = new RandomPrimeInt().getRandom();
+            q = new RandomPrimeInt().getRandom();
+            n = BigInteger.valueOf(p * q);
+            fn = (p-1)*(q-1);
+            e = 0;
+            d = 0;
+            for (int i = 3; i < fn; i++){
+                e = i;
+                if (GCD.gcd(e,fn)==1)
+                    break;
+            }
+            for(int i=0;i<=9;i++)
+            {
+                int x = 1+(i*fn);
+                if(x%e==0)
+                {
+                    d=x/e;
+                    break;
+                }
+            }
+            System.out.println("p = " + p + " and q = " + q + " n = " + n + " fn = " + fn + " e = " + e + " d = " + d);
+            while (msg.compareTo(n) == 1){
+                System.out.println("Message have to be less than " + n + ". Enter other message!");
+                msg = BigInteger.valueOf(scanner.nextInt());
+            }
             BigInteger c = msg.modPow(BigInteger.valueOf(e),n);
             System.out.println("Encrypted message (c) is " + c);
             BigInteger m = c.modPow(BigInteger.valueOf(d),n);
